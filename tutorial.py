e8b961cbb9e4fa50a6c7d398ff672ae82075cadc
aust <chiwmaniaustine@gmail.com>
2021-09-05 10:06:38 -0400

    Initial commit

---
+++ tutorial.py
@@ -0,0 +1,24 @@
+print("welcome to my channel")
+name = input("Enter Your username: ")
+password_ = input("Enter Your Password: ")
+name_ = 'austine'
+password = '123e'
+
+
+def program():
+    weather = 'cold'
+    day = 'monday'
+    if weather == 'sunny':
+        print("nice day")
+    else:
+        print("its a bad day")
+    if day == 'sunday':
+        print("We will be attending church today")
+    else:
+        print("Not going to church today its on " + day)
+
+
+if name == name and password_ == password:
+    program()
+else:
+    print(name + password_+" Details does not much!!")
